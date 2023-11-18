d7725aba3d26a00a696b6b586a08fbbec937a6ee
gaeaf <150544298+gaeaf@users.noreply.github.com>
2023-11-18 20:28:17 +0800

    Update feature_extraction.py

--- first_flask/feature_extraction.py
+++ first_flask/feature_extraction.py
@@ -1,3 +1,6 @@
+import socket
+import urllib.parse
+
 def get_char_count(url, char):
     """
     Takes an input string url and string char and returns the number of times the input char is seen in the url.
@@ -10,9 +13,19 @@ def get_char_count(url, char):
 
 def get_word_presence(url, word):
     """
-    Takes an input string url and string word and returns a 1 if the word is present in the url, 0 otherwise.
+    Takes an input string url and string word and returns a 1 if the word is present in the url and 0 otherwise.
     """
     if word in url:
         return 1
     else:
         return 0
+    
+def get_ip_presence(url):
+    """
+    Takes an input string url and returns its IP address.
+    """
+    parsed_url = urllib.parse.urlparse(url)
+    return(socket.gethostbyname(parsed_url.netloc))
+
+if __name__ == "__main__":
+    print(get_ip_presence("https://www.kaggle.com/datasets/sid321axn/malicious-urls-dataset/data"))
