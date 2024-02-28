06ba681facdbbc760b19b4a05d1f3a5082c80d4b
gaeaf <150544298+gaeaf@users.noreply.github.com>
2024-02-28 21:59:10 +0800

    Update filter_feature_selection.py

--- 02_feature-engineering/filter_feature_selection.py
+++ 02_feature-engineering/filter_feature_selection.py
@@ -25,7 +25,7 @@ feature_anova_stat = feature_anova_stat.sort_values(ascending = False)
 feature_anova_p = pd.Series(anova_p)
 feature_anova_p.index = numerical_features.columns
 pd.option_context('display.max_rows', None, 'display.max_columns', None)
-print(feature_anova_stat.index.to_string())
+print(feature_anova_stat.to_string())
 print(feature_anova_p)
 
 chi2_stats, chi2_p = chi2(categorical_features, url_class)
@@ -40,7 +40,7 @@ feature_chi2_stat = feature_chi2_stat.sort_values(ascending = False)
 feature_chi2_p = pd.Series(chi2_p)
 feature_chi2_p.index = categorical_features.columns
 pd.option_context('display.max_rows', None, 'display.max_columns', None)
-print(feature_chi2_stat.index.to_string())
+print(feature_chi2_stat.to_string())
 print(feature_chi2_p)
 
 corr = input_features.corr()
