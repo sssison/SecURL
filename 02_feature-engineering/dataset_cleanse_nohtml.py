6cd188a485c5f963bc573e295943207ac2d901ea
gaeaf <150544298+gaeaf@users.noreply.github.com>
2024-02-28 21:43:54 +0800

    Add files via upload

---
+++ 02_feature-engineering/dataset_cleanse_nohtml.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from feature_generation_content_function_htmlin import get_html
+
+dataset = pd.read_csv('dataset_splitN.csv') #REPLACE N WITH YOUR SPLIT
+
+dataset['type'] = dataset['type'].replace({
+    'benign' : 0,
+    'defacement': 1,
+    'phishing': 2,
+    'malware': 3,
+})
+
+dataset['html'] = dataset['url'].apply(lambda x: get_html(x))
+dataset = dataset.dropna()
+
+dataset.to_csv("dataset_splitN_cleansed.csv", encoding='utf-8', index=False) #REPLACE N WITH YOUR SPLIT
