e53225daa3632c58579ba20159300e1dc7b2feff
conluiben <conradoluizbencio@gmail.com>
2024-05-09 09:59:19 +0800

    UI Feedback Integration
    - background.js: rejects URLs that don't start with HTTP/HTTPS
    - background.js: edited logic so that after checking that URL is NOT in blacklist, check with model of backend server and retrieve results
    - content.js: fixed onload event not working (replaced with named async function with message listener)
    - content.js: added script for displaying notification with animation (now triggerable anytime with createNotif function)
    - added content.css for notif. BUG: import fonts not working!

--- securl_eee/scripts/background.js
+++ securl_eee/scripts/background.js
@@ -15,13 +15,19 @@ chrome.runtime.onInstalled.addListener(function(details) {
 });
 
 chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
+    // ! accept only HTTP and HTTPS URLs
+    if(!details.url || !details.url.startsWith('http')){
+        return;
+    }
+
     // Check if it's the main frame navigation
     console.log("details");
     console.log(details)
     var redirectUrls, blacklists;
     var isMaliciousUrl = false;
+    var response_json;
 
-    chrome.storage.local.get(["redirectUrls","blacklist"], function(data) {
+    chrome.storage.local.get(["redirectUrls","blacklist"], async function(data) {
         redirectUrls = data.redirectUrls || {};
         blacklists = data.blacklist || {};
 
@@ -40,10 +46,11 @@ chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
             }
 
             // ! check also whether the URL is malicious or not using the service
-            // TODO: (see above)
+            // TODO: (see above). parameters: URL, isSecure (configure this!)
+            // ? already done AFTER checking whether URL skipped or not
             
-            // ! update the URL of the tabId
-            // redirectUrls[details.tabId] = details.url;
+            //// update the URL of the tabId ???
+            //// redirectUrls[details.tabId] = details.url;
             
             var redirectUrlUpdated;
 
@@ -65,6 +72,12 @@ chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
             if (redirectUrlUpdated["skipped"]){
                 isMaliciousUrl = false;
                 redirectUrlUpdated["skipped"] = false;
+            } else if (!isMaliciousUrl) { // TODO: <efficiency> only check with the model if NOT skipped and NOT in blacklist so far
+                response_json = await checkIfMaliciousUrl(details.url,false);
+                isMaliciousUrl = (response_json["message"]!=="Benign");
+                redirectUrlUpdated["serverResult"] = response_json;     // store the entire JSON result in serverResult property
+                console.log("Checked the URL with the model to get ff. result...");
+                console.log(response_json);
             }
             
             redirectUrls[details.tabId] = JSON.parse(JSON.stringify(redirectUrlUpdated));
@@ -86,3 +99,46 @@ chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId){
     console.log("onReplaced triggered");
     console.log(`added Tab: ${addedTabId} and removed Tab: ${removedTabId}`);
 });
+
+// ! Listen for message to trigger the notification banner, if applicable only
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension");
+        if (request.hasOwnProperty("message") && request.message === "tab_ready"){
+            console.log("Received signal with message tab_ready");
+            sendResponse({action: "open_notif"});
+        } else {
+            // console.log("Received no greeting but Hi.");
+        }
+    }
+);
+
+async function checkIfMaliciousUrl(currentUrl, isEnhancedSec){
+    var serverUrl = "http://10.158.66.12:5000/securl?";
+    var url = serverUrl + new URLSearchParams({
+        inp_url: currentUrl,
+        is_secure: (isEnhancedSec ? "enabled" : "disabled")
+    });
+    // console.log(url);
+
+    var response_json = null;
+    await fetch(url, {
+        method: 'GET',
+    })
+        .then(function (data) {
+            // console.log('Request succeeded with JSON response');
+            return data.json(); //call only ONCE (affects data variable)
+            // shortcut: // .then(response => response.json())
+        })
+        .then(function(data_json){
+            response_json = data_json;
+            // console.log(response_json);
+        })
+        .catch(function (error) {
+            console.log('Request failed', error);
+        });
+    
+    return response_json;
+}
--- securl_eee/scripts/content.js
+++ securl_eee/scripts/content.js
@@ -44,22 +44,68 @@ chrome.storage.local.get(['randomNumber','redirectUrls'], function (result) {
     // }
   });
 
-document.addEventListener("DOMContentLoaded", function () {
+// window.addEventListener("DOMContentLoaded", async function () {
+if (document.readyState !== "loading") {
+    initializeContentScript(); // Or setTimeout(onReady, 0); if you want it consistently async
+} else {
+    document.addEventListener("DOMContentLoaded", initializeContentScript);
+}
+// document.addEventListener("DOMContentLoaded", initializeContentScript);
+
+
+async function initializeContentScript() {
     // get the current Tab ID
-    chrome.tabs.getCurrent(function(tab) {
-        // tabs[0] will contain information about the currently active tab
-        const currentTabId = tab.id;
-        console.log("Current tab ID:", currentTabId);
-        // Retrieve malicious URL from storage
-        chrome.storage.local.get(["redirectUrls"], function (result) {
-            const maliciousURL = result.redirectUrls[currentTabId];
-            // Populate button with malicious URL
-            document.getElementById("proceedButton").addEventListener("click", function () {
-                // Redirect user to the detected malicious URL
-                window.location.href = maliciousURL;
-            });
-        });
-    });
+    // var currentTabId = "12345";
+
+    // chrome.tabs.getCurrent(function(tab) {
+    //     // tabs[0] will contain information about the currently active tab
+    //     currentTabId = tab.id;
+    //     console.log("Current tab ID:", currentTabId);
+    //     // Retrieve malicious URL from storage
+    //     chrome.storage.local.get(["redirectUrls"], function (result) {
+    //         const maliciousURL = result.redirectUrls[currentTabId];
+    //         // Populate button with malicious URL
+    //         document.getElementById("proceedButton").addEventListener("click", function () {
+    //             // Redirect user to the detected malicious URL
+    //             window.location.href = maliciousURL;
+    //         });
+    //     });
+    // });
+
+    
+    // ? Listen for message to trigger the notification banner, if applicable only\
+    // TODO: remove this listener if not necessary
+    // ! IMPORTANT: sendResponse doesnt trigger the onMessage listener. Instead, it goes to the *response* variable.
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+                if (request.hasOwnProperty("action") && request.action === "open_notif"){
+                    // sendResponse({farewell: "goodbye"});
+                    console.log("Received a signal to trigger the notification. Go!");
+                    createNotif();
+                } else {
+                    console.log("Received no greeting but Hi.");
+                    
+                }
+            }
+        );
+        
+    // ! send the message to the background script stating that we're ready to trigger the notification
+    const response = await chrome.runtime.sendMessage({message: "tab_ready"});
+    console.log("We sent a message! See the response:");
+    console.log(response);
+
+    // ! trigger the notification (and other necessary actions) here depending on response of server!
+    if (response.hasOwnProperty("action") && response.action === "open_notif"){
+        // sendResponse({farewell: "goodbye"});
+        console.log("Received a signal to trigger the notification. Go!");
+        createNotif();
+    } else {
+        console.log("Received no greeting but Hi.");
+        
+    }
 
     // // Event listener for exit button
     // document.getElementById("exitButton").addEventListener("click", function () {
@@ -68,20 +114,47 @@ document.addEventListener("DOMContentLoaded", function () {
     //         chrome.tabs.remove(tab.id);
     //     });
     // });
-});
+}
 
-// // ! Listen for event that URL was detected as malicious
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         console.log(sender.tab ?
-//                     "from a content script:" + sender.tab.url :
-//                     "from the extension");
-//         if (request.hasOwnProperty("greeting") && request.greeting === "hello"){
-//             // sendResponse({farewell: "goodbye"});
-//             console.log("Received a malicious URL warning. Hi.");
-//         } else {
-//             console.log("Received no greeting but Hi.");
-            
-//         }
-//     }
-// );
+function createNotif(){
+    // If notification already exists, delete!
+    var notifBox = document.querySelector("div.notif-box");
+    if (notifBox){
+        notifBox.remove();
+    }
+
+    // Create the div element
+    var divElement = document.createElement('div');
+    divElement.classList.add('notif-box');
+
+    var h2Element = document.createElement('h2');
+    h2Element.textContent = 'URL Report Success';
+    divElement.appendChild(h2Element);
+
+    var pElement = document.createElement('p');
+    pElement.textContent = 'Successfully sent the report to the server for feedback!';
+    divElement.appendChild(pElement);
+
+    // Append the div element to the body
+    document.body.appendChild(divElement);
+
+    // TODO: animate the notification box
+    document.querySelector("div.notif-box").style.display = "flex";
+    document.querySelector("div.notif-box").style.transition = "right 1s ease, opacity 0.3s linear";
+    document.querySelector("div.notif-box").classList.add("notransition");
+    document.querySelector("div.notif-box").style.right = `${-document.querySelector("div.notif-box").offsetWidth-20}px`;
+    document.querySelector("div.notif-box").style.visibility = `visible`;
+    document.querySelector("div.notif-box").style.opacity = `1`;
+    setTimeout(function(){
+        document.querySelector("div.notif-box").classList.remove("notransition");
+        document.querySelector("div.notif-box").style.right = `20px`;
+    },10);
+    setTimeout(function(){
+        document.querySelector("div.notif-box").style.opacity = `0`;
+        // document.querySelector("div.notif-box").remove();
+    },3000);
+    
+}
+// setInterval(createNotif,6000);
+
+// createNotif();
