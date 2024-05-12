7ede5478ff3c0c34f6a52d357e45ab14578cb457
SupernovaExe <63900699+SupernovaExe@users.noreply.github.com>
2024-05-13 00:27:41 +0800

    UI Changes (options page)
    - Updated UI
    - TODO: update save button

--- securl_eee/options/options.js
+++ securl_eee/options/options.js
@@ -70,5 +70,20 @@ const saveOptions = () => {
   );
 };
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
+
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save-options').addEventListener('click', saveOptions);
